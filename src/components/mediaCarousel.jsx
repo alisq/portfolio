13d86c07298e88a47a7b9760f21a0d59f50a96fc
alisq <ali.shamas.qadeer@gmail.com>
2025-09-23 20:52:24 +0100

    added basic mobile.

--- src/components/mediaCarousel.jsx
+++ src/components/mediaCarousel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Flickity from "react-flickity-component";
-import "flickity/css/flickity.css"; // make sure you import the styles
+import "flickity/css/flickity.css";
 
 const defaultFlickityOptions = {
   cellAlign: "left",
@@ -9,38 +9,46 @@ const defaultFlickityOptions = {
   imagesLoaded: true,
   initialIndex: 1,
   wrapAround: true,
-
 };
 
 function MediaCarousel({ pics = [], flickityOptions = {} }) {
   const options = { ...defaultFlickityOptions, ...flickityOptions };
 
-  if (!Array.isArray(pics) || pics.length === 0) {
-    return null; // nothing to show
-  }
+  if (!Array.isArray(pics) || pics.length === 0) return null;
 
-  return pics.length > 1 ? (
-    <Flickity
-      className={"carousel"}
-      elementType={"div"}
-      options={options}
-      disableImagesLoaded
-      reloadOnUpdate
-      static
-    >
-      {pics.map((pic, i) => (
-        <img
-          key={i}
-          src={`https://alisq.github.io/portfolio/public/media/${pic.url}`}
-          alt={pic.alt || `Artwork image ${i + 1}`}
-        />
-      ))}
-    </Flickity>
-  ) : (
-    <img
-      src={`https://alisq.github.io/portfolio/public/media/${pics[0].url}`}
-      alt={pics[0].alt || "Artwork image"}
-    />
+  return (
+    <>
+      {/* Flickity version (for wide screens) */}
+      {pics.length > 1 && (
+        <Flickity
+          className={"carousel big-carousel"}
+          elementType={"div"}
+          options={options}
+          disableImagesLoaded
+          reloadOnUpdate
+          static
+        >
+          {pics.map((pic, i) => (
+            <img
+              key={i}
+              src={`https://alisq.github.io/portfolio/public/media/${pic.url}`}
+              alt={pic.alt || `Artwork image ${i + 1}`}
+            />
+          ))}
+        </Flickity>
+      )}
+
+      {/* Simple inline version (for small screens) */}
+      <div className="small-carousel">
+        {pics.map((pic, i) => (
+          <img
+            key={i}
+            src={`https://alisq.github.io/portfolio/public/media/${pic.url}`}
+            alt={pic.alt || `Artwork image ${i + 1}`}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
